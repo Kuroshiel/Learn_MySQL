a3030bd9ca3641f9997a29d4188b20fa337d08d8
Kuroshiel <alifianfikry@gmail.com>
2023-12-30 14:13:40 +0700

    Learn Numeric Function

--- MySQL_Dasar/Table_Products.sql
+++ MySQL_Dasar/Table_Products.sql
@@ -158,3 +158,17 @@ SELECT * FROM products ORDER BY id LIMIT 10, 5;
 SELECT category FROM products;
 
 SELECT DISTINCT category FROM products;
+
+SELECT 10, 'x', 10, 10 * 10 as 'Hasil';
+
+SELECT id, name, price, price DIV 1000 AS 'Price in K' FROM products;
+
+
+SELECT POWER (10, 2);
+
+SELECT PI ();
+
+SELECT id, COS(price), SIN(price), TAN(price) FROM products;
+
+SELECT id, name, price FROM products
+WHERE price DIV 1000 > 15;
