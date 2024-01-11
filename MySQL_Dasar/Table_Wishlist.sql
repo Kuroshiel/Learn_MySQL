605da5885e9f12cc38e539c72a0362ced3226458
Kuroshiel <alifianfikry@gmail.com>
2024-01-11 15:17:08 +0700

    Learn Join

--- MySQL_Dasar/Table_Wishlist.sql
+++ MySQL_Dasar/Table_Wishlist.sql
@@ -36,3 +36,43 @@ ALTER TABLE wishlist
             
 INSERT INTO wishlist(id_product, description)
 VALUES ('Pxxxx', 'Makanan Kesukaan');
+
+SELECT * FROM wishlist
+JOIN products ON (wishlist.id_product = products.id);
+
+SELECT products.id, products.name, wishlist.description
+FROM wishlist
+JOIN products ON (products.id = wishlist.id_product);
+
+SELECT 
+    w.id AS id_wishlist,
+    p.id AS id_product,
+    p.name AS product_name,
+    w.description AS wishlist_description
+FROM wishlist AS w
+JOIN products AS p ON (p.id = w.id_product); 
+
+DESC customers;
+
+ALTER TABLE wishlist
+	ADD COLUMN id_customer INT;
+    
+ALTER TABLE wishlist
+ADD CONSTRAINT fk_wishlist_customer 
+FOREIGN KEY (id_customer) REFERENCES customers (id);
+
+SELECT * FROM customers;
+
+UPDATE wishlist
+SET id_customer = 1
+WHERE id = 2;
+
+SELECT customers.email, products.id, products.name, wishlist.description
+FROM wishlist
+JOIN products ON (products.id = wishlist.id_product)
+JOIN customers ON (customers.id = wishlist.id_customer);
+
+SELECT c.email, p.id, p.name, w.description
+FROM wishlist AS w
+JOIN products AS p ON (p.id = w.id_product)
+JOIN customers AS c ON (c.id = w.id_customer);
