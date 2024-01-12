7330c56af03ae064ae32cb1ac69edeca46333a40
Kuroshiel <alifianfikry@gmail.com>
2024-01-12 14:23:11 +0700

    Learn Subquery

--- MySQL_Dasar/Table_Products.sql
+++ MySQL_Dasar/Table_Products.sql
@@ -294,3 +294,18 @@ INSERT INTO products (id, name, price, quantity)
 VALUES  ('X0001', 'X Satu', 1000, 1000),
 		('X0002', 'X Dua', 1000, 1000),
 		('X0003', 'X Tiga', 1000, 1000);
+        
+SELECT * FROM products
+WHERE price > (SELECT AVG(price) FROM products);
+
+SELECT MAX(price) FROM products;
+
+SELECT * FROM products;
+
+UPDATE products
+SET price = 1000000
+WHERE id = 'X0003'; 
+
+SELECT MAX(cp.price)
+FROM (SELECT price FROM categories
+		INNER JOIN products ON (products.id_category = categories.id)) AS cp;
